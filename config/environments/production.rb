5f6e3ea7012ef1cea21074d620bc68064756e02a
SarveshRajoba <132903409+SarveshRajoba@users.noreply.github.com>
2025-10-19 00:03:48 +0530

    Update host configuration to allow all onrender.com hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,6 +86,7 @@ Rails.application.configure do
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
-  config.hosts << "infovault-1.onrender.com"
+  config.hosts << /.*\.onrender\.com/
+
 
 end
